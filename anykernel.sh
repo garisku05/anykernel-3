e0c797bf669ef3c811a5fbf376439223277f9c33
Fadly Andriyanto <58070697+andeh24@users.noreply.github.com>
2020-05-17 10:05:08 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Welcome to the Undead World!
+kernel.string=Selamat menjalankan ibadah puasa, sar :)
 do.devicecheck=1
 do.modules=0
 do.systemless=0
