8fd9c330bedd2fd40c113851af82ed7c9893e928
mgs28-mh <55727892+mgs28-mh@users.noreply.github.com>
2020-06-19 23:21:18 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=4 U Kernel, brought to you by @andeh24
+kernel.string=Mgs+ Kernel, special for you by @garisku05
 do.devicecheck=1
 do.modules=0
 do.systemless=0
